584d9dc7561122d6203fc366bf7f37bd65fcd86c
Akim <akim.bartsits@mail.ru>
2025-04-14 16:40:41 +0300

    Отредактирован вывод команды help

--- internal/app/command/help.go
+++ internal/app/command/help.go
@@ -16,14 +16,14 @@ func (h *HelpCommand) Execute() error {
 		h.Writer = os.Stdout
 	}
 
-	fmt.Fprintln(h.Writer, "\nКоманды CLI:")
+	fmt.Fprintln(h.Writer, "======================\nКоманды CLI======================")
 	fmt.Fprintln(h.Writer, "  go run ./cmd list             - Показать список задач")
 	fmt.Fprintln(h.Writer, `  go run ./cmd add "Задача"     - Добавить новую задачу`)
 	fmt.Fprintln(h.Writer, "  go run ./cmd delete ID        - Удалить задачу по ID")
 	fmt.Fprintln(h.Writer, "  go run ./cmd done ID          - Отметить задачу выполненной")
 	fmt.Fprintln(h.Writer, "  go run ./cmd help             - Показать справку")
 
-	fmt.Fprintln(h.Writer, "API-команды:")
+	fmt.Fprintln(h.Writer, "======================\nКоманды API======================")
 	fmt.Fprintln(h.Writer, "  GET     /todos                - Получить все задачи (JSON)")
 	fmt.Fprintln(h.Writer, `  POST    /add {"title":"..."}`+"   - Добавить задачу")
 	fmt.Fprintln(h.Writer, "  DELETE  /delete?id=ID         - Удалить задачу по ID")
